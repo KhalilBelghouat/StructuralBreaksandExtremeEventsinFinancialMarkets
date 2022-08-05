6c1203489eb6373508de31d730e2c8399f00a60e
Khalil Belghouat <67176135+KhalilBelghouat@users.noreply.github.com>
2022-08-05 21:42:25 +0100

    Add files via upload

---
+++ descriptive_statistics/descriptive_statistics.R
@@ -0,0 +1,8 @@
+# SUPPLEMENTARY CODE FOR THE MASTER THESIS: 
+# Structural Breaks and Extreme Events in Financial Markets
+
+# This script computes summary statistics about the MASI data.
+
+# Summary descriptive statistics of the MASI.
+summary <- summary(MASI)
+describe <- describe(MASI)
